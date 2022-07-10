46e3f2153f443334a1eb35823a9079c8e283e33a
Nasser Kilesse <nasser.kilesse@ryvit.com>
2022-07-10 19:16:34 -0300

    feature: added menu side bar

--- src/components/DataflixApp.vue
+++ src/components/DataflixApp.vue
@@ -1,6 +1,16 @@
 <template>
-  <v-app-bar absolute color="primary" dark scroll-target="#scrolling-techniques-2">
-    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+  <v-container
+    class="mx-auto overflow-hidden"
+    height="400"
+    width="400"
+  >
+  <v-app-bar 
+    absolute
+    color="primary" 
+    dark 
+    scroll-target="#scrolling-techniques-2"
+    >
+    <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
     <v-app-bar-title>Dataflix</v-app-bar-title>
     <v-spacer></v-spacer>
     <v-btn icon>
@@ -10,6 +20,40 @@
       <v-icon>mdi-dots-vertical</v-icon>
     </v-btn>
   </v-app-bar>
+  <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list-item>
+        <v-list-item-avatar>
+          <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title>John Leider</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    </v-container>
 </template>
 
 <script lang="ts">
@@ -20,10 +64,19 @@ export default Vue.extend({
   components: {
   },
   data: () => ({
-
+    drawer: false,
+    items: [
+          { title: 'Home', icon: 'mdi-view-dashboard' },
+          { title: 'About', icon: 'mdi-forum' },
+    ],
+    right: null,
   }),
   methods: {
-  }
-
+  },
+  watch: {
+      group () {
+        this.drawer = false
+      },
+  },
 })
 </script>
